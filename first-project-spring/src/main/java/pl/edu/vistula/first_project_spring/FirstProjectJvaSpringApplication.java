36574dc7b55547217fcff47181e9eed8fb20f2a6
PirateMachine <Wiki20221@gmail.com>
2025-01-22 12:34:37 +0100

    Initial commit

---
+++ first-project-spring/src/main/java/pl/edu/vistula/first_project_spring/FirstProjectJvaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJvaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectJvaSpringApplication.class, args);
+	}
+
+}
